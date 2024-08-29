b621a65d02e80e66239bec5630124e8f41aa5669
Maxwell Rubert <137780966+Maxwellrubert@users.noreply.github.com>
2024-08-29 23:38:41 +0530

    Update d01inout.py

--- d01inout.py
+++ d01inout.py
@@ -50,7 +50,7 @@ Hello InterviewBit'''
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Maxwell Rubert - Aspiring Data Scientist</title>
+    <title>Maxwell Rubert - Data Scientist</title>
     <style>
         body {
             font-family: Arial, sans-serif;
